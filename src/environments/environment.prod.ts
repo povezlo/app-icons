63cc492bc058213ea062d4f5babd20f1a40b91a4
povezlo <aliester.crowly@gmail.com>
2020-07-27 11:08:53 +0300

    delete apikey in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import {Environment} from './interface';
 
 export const environment: Environment = {
-  apiKey: 'AIzaSyDuv2YV-jme3h8OvtZC-HwhZF8NoW6YYDQ',
+  apiKey: '',
   production: true,
   firebaseDataBase: 'https://ikony-6f09b.firebaseio.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 import {Environment} from './interface';
 
 export const environment: Environment = {
-  apiKey: 'AIzaSyDuv2YV-jme3h8OvtZC-HwhZF8NoW6YYDQ',
+  apiKey: '',
   production: false,
   firebaseDataBase: 'https://ikony-6f09b.firebaseio.com/'
 };
